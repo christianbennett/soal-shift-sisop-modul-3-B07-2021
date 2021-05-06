5f1c5540eab6c515e6513b34e22813f241c5fa1c
christianbennett <christianbennettrobin@gmail.com>
2021-05-06 14:37:10 +0700

    update c

--- soal2/soal2c.c
+++ soal2/soal2c.c
@@ -12,47 +12,47 @@ int main()
 
     if (fork() == 0)
     {
-        //close read dari pipe A
+        //close read dari pipe1
         close(pipe1[0]);
         dup2(pipe1[1], 1);
-        //do not pass A-write twice
+        //close read dari pipe1
         close(pipe1[1]);
 
         execlp("/bin/ps", "ps", "aux", NULL);
     }
-
-    close(pipe1[1]); // A-write not needed anymore
-
-    pipe(pipe2); //do not create this pipe until needed
+    // close write dari pipe1
+    close(pipe1[1]);
+    //do not create this pipe until needed
+    pipe(pipe2);
 
     if (fork() == 0)
     {
-        //close read dari pipe B
+        //close read dari pipe2
         close(pipe2[0]);
 
         dup2(pipe1[0], 0);
-        //do not pass A-read twice
+        //close read dari pipe1
         close(pipe1[0]);
         dup2(pipe2[1], 1);
-        //do not pass B-write twice
+        //close write dari pipe2
         close(pipe2[1]);
 
         execlp("/usr/bin/sort", "sort", "-nrk", "3,3", NULL);
     }
-    // A-read not needed anymore
+    //close read dari pipe1
     close(pipe1[0]);
-    // B-write not needed anymore
+    //close write dari pipe2
     close(pipe2[1]);
 
     if (fork() == 0)
     {
         dup2(pipe2[0], 0);
-        // do not pass B-read twice
+        //close read dari pipe2
         close(pipe2[0]);
 
         execlp("/usr/bin/head", "head", "-5", NULL);
     }
-    // B-read not needed anymore
+    //close read dari pipe2
     close(pipe2[0]);
     return 0;
 }
